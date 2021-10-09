6332bfc82f6851f8361dd315055a6e302a689d1e
mvm11 <mvalenciaminota@gmail.com>
2021-10-09 06:15:49 -0500

    The initial heroes data has been added

--- src/componentes/Heroes.js
+++ src/componentes/Heroes.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import Hero from './Hero'
-
+import React from "react";
+import Hero from "./Hero";
 
 export const Heroes = () => {
-    return (
-        <div>
-            <Hero/>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4"></div>
+        <Hero />
+      </div>
+    </div>
+  );
+};
 
 export default Heroes;
