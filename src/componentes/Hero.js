6a18ec4e54b83fddad489169cfa1edded64476a6
mvm11 <mvalenciaminota@gmail.com>
2021-10-09 07:34:11 -0500

    The Hero component has been updated

--- src/componentes/Hero.js
+++ src/componentes/Hero.js
@@ -1,21 +1,23 @@
 import React from "react";
 
-
-export const Hero = ({hero}) => {
-
-
+export const Hero = ({ hero }) => {
+  const isFavorite = false;
   return (
-    <div className="card">
+    <div className="card m-4">
       <img
         src={hero.imageUrl}
         alt={hero.superhero}
         className="card-img-top"
       ></img>
       <div className="card-body">
-          <h4>{hero.superhero}</h4>
-          <button className="btn btn-outline-primary">
-              favorite 
-          </button>
+        <h4>{hero.superhero}</h4>
+        <button
+          className={`btn ${
+            isFavorite ? "btn-success" : "btn-outline-primary"
+          } `}
+        >
+          favorite
+        </button>
       </div>
       <h1>Hero</h1>
     </div>
