a613539e033005658b7ba1d9dd202bc6044e10bd
mvm11 <mvalenciaminota@gmail.com>
2021-10-09 06:25:16 -0500

    Heroes component has been updated

--- src/componentes/Heroes.js
+++ src/componentes/Heroes.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Hero from "./Hero";
+import initialHeroes from "../consts/initialHeroes";
 
 export const Heroes = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-4"></div>
-        <Hero />
+        {initialHeroes.map((hero) => (
+          <div className="col-md-4" key={hero.id}>
+            <Hero 
+            hero={hero}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/consts/initialHeroes.js
+++ src/consts/initialHeroes.js
@@ -1,84 +1,5 @@
-export const heroes = [
-    {
-        'id': 'dc-batman',
-        'superhero':'Batman', 
-        'publisher':'DC Comics', 
-        'alter_ego':'Bruce Wayne',
-        'first_appearance':'Detective Comics #27',
-        'characters':'Bruce Wayne'
-    },
-    {
-        'id': 'dc-superman',
-        'superhero':'Superman', 
-        'publisher':'DC Comics', 
-        'alter_ego':'Kal-El',
-        'first_appearance':'Action Comics #1',
-        'characters':'Kal-El'
-    },
-    {
-        'id': 'dc-flash',
-        'superhero':'Flash', 
-        'publisher':'DC Comics', 
-        'alter_ego':'Jay Garrick',
-        'first_appearance':'Flash Comics #1',
-        'characters':'Jay Garrick, Barry Allen, Wally West, Bart Allen'
-    },
-    {
-        'id': 'dc-green',
-        'superhero':'Green Lantern', 
-        'publisher':'DC Comics', 
-        'alter_ego':'Alan Scott',
-        'first_appearance':'All-American Comics #16',
-        'characters':'Alan Scott, Hal Jordan, Guy Gardner, John Stewart, Kyle Raynor, Jade, Sinestro, Simon Baz'
-    },
-    {
-        'id': 'dc-arrow',
-        'superhero':'Green Arrow', 
-        'publisher':'DC Comics', 
-        'alter_ego':'Oliver Queen',
-        'first_appearance':'More Fun Comics #73',
-        'characters':'Oliver Queen'
-    },
-    {
-        'id': 'dc-wonder',
-        'superhero':'Wonder Woman', 
-        'publisher':'DC Comics', 
-        'alter_ego':'Princess Diana',
-        'first_appearance':'All Star Comics #8',
-        'characters':'Princess Diana'
-    },
-    {
-        'id': 'dc-martian',
-        'superhero':'Martian Manhunter', 
-        'publisher':'DC Comics', 
-        'alter_ego':'J\'onn J\'onzz',
-        'first_appearance':'Detective Comics #225',
-        'characters':'Martian Manhunter'
-    },
-    {
-        'id': 'dc-robin',
-        'superhero':'Robin/Nightwing', 
-        'publisher':'DC Comics', 
-        'alter_ego':'Dick Grayson',
-        'first_appearance':'Detective Comics #38',
-        'characters':'Dick Grayson'
-    },
-    {
-        'id': 'dc-blue',
-        'superhero':'Blue Beetle', 
-        'publisher':'DC Comics', 
-        'alter_ego':'Dan Garret',
-        'first_appearance':'Mystery Men Comics #1',
-        'characters':'Dan Garret, Ted Kord, Jaime Reyes'
-    },
-    {
-        'id': 'dc-black',
-        'superhero':'Black Canary', 
-        'publisher':'DC Comics', 
-        'alter_ego':'Dinah Drake',
-        'first_appearance':'Flash Comics #86',
-        'characters':'Dinah Drake, Dinah Lance'
-    },
+export const initialHeroes = [
+
     {
         'id': 'marvel-spider',
         'superhero':'Spider Man', 
@@ -159,4 +80,6 @@ export const heroes = [
         'first_appearance':'The Fantastic Four #48',
         'characters':'Norrin Radd'
     }
-]
+];
+
+export default initialHeroes;
