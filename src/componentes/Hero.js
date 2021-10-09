48a9471af1a123cf430050dc183e1315244df95c
mvm11 <mvalenciaminota@gmail.com>
2021-10-09 07:22:50 -0500

    The initialHeroes data JSON file has been updated

--- src/componentes/Hero.js
+++ src/componentes/Hero.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export const Hero = () => {
-    return (
-        <div>
-            <h1>Hero</h1>
-        </div>
-    )
-}
+
+export const Hero = ({hero}) => {
+
+
+  return (
+    <div className="card">
+      <img
+        src={hero.imageUrl}
+        alt={hero.superhero}
+        className="card-img-top"
+      ></img>
+      <div className="card-body">
+          <h4>{hero.superhero}</h4>
+          <button className="btn btn-outline-primary">
+              favorite 
+          </button>
+      </div>
+      <h1>Hero</h1>
+    </div>
+  );
+};
 
 export default Hero;
--- src/consts/initialHeroes.js
+++ src/consts/initialHeroes.js
@@ -6,39 +6,17 @@ export const initialHeroes = [
         'publisher':'Marvel Comics', 
         'alter_ego':'Peter Parker',
         'first_appearance':'Amazing Fantasy #15',
-        'characters':'Peter Parker'
+        'characters':'Peter Parker',
+        'imageUrl': './assets/heroes/marvel-spider.jpg'
     },
     {
-        'id': 'marvel-captain',
-        'superhero':'Captain America', 
-        'publisher':'Marvel Comics', 
-        'alter_ego':'Steve Rogers',
-        'first_appearance':'Captain America Comics #1',
-        'characters':'Steve Rogers'
-    },
-    {
-        'id': 'marvel-iron',
-        'superhero':'Iron Man', 
-        'publisher':'Marvel Comics', 
-        'alter_ego':'Tony Stark',
-        'first_appearance':'Tales of Suspense #39',
-        'characters':'Tony Stark'
-    },
-    {
-        'id': 'marvel-thor',
-        'superhero':'Thor', 
-        'publisher':'Marvel Comics', 
-        'alter_ego':'Thor Odinson',
-        'first_appearance':'Journey into Myster #83',
-        'characters':'Thor Odinson'
-    },
-    {
-        'id': 'marvel-hulk',
-        'superhero':'Hulk', 
-        'publisher':'Marvel Comics', 
-        'alter_ego':'Bruce Banner',
-        'first_appearance':'The Incredible Hulk #1',
-        'characters':'Bruce Banner'
+        'id': 'dc-batman',
+        'superhero':'Batman', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Bruce Wayne',
+        'first_appearance':'Detective Comics #27',
+        'characters':'Bruce Wayne',
+        'imageUrl':'./assets/heroes/dc-batman.jpg'
     },
     {
         'id': 'marvel-wolverine',
@@ -46,23 +24,35 @@ export const initialHeroes = [
         'publisher':'Marvel Comics', 
         'alter_ego':'James Howlett',
         'first_appearance':'The Incredible Hulk #180',
-        'characters':'James Howlett'
+        'characters':'James Howlett',
+        'imageUrl': './assets/heroes/marvel-wolverine.jpg'
     },
     {
-        'id': 'marvel-daredevil',
-        'superhero':'Daredevil', 
-        'publisher':'Marvel Comics', 
-        'alter_ego':'Matthew Michael Murdock',
-        'first_appearance':'Daredevil #1',
-        'characters':'Matthew Michael Murdock'
+        'id': 'dc-robin',
+        'superhero':'Robin/Nightwing', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Dick Grayson',
+        'first_appearance':'Detective Comics #38',
+        'characters':'Dick Grayson',
+        'imageUrl': './assets/heroes/dc-robin.jpg'
     },
     {
-        'id': 'marvel-hawkeye',
-        'superhero':'Hawkeye', 
-        'publisher':'Marvel Comics', 
-        'alter_ego':'Clinton Francis Barton',
-        'first_appearance':'Tales of Suspense #57',
-        'characters':'Clinton Francis Barton'
+        'id': 'dc-wonder',
+        'superhero':'Wonder Woman', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Princess Diana',
+        'first_appearance':'All Star Comics #8',
+        'characters':'Princess Diana',
+        'imageUrl': './assets/heroes/dc-wonder.jpg'
+    },
+    {
+        'id': 'dc-arrow',
+        'superhero':'Green Arrow', 
+        'publisher':'DC Comics', 
+        'alter_ego':'Oliver Queen',
+        'first_appearance':'More Fun Comics #73',
+        'characters':'Oliver Queen',
+        'imageUrl': './assets/heroes/dc-arrow.jpg'
     },
     {
         'id': 'marvel-cyclops',
@@ -70,16 +60,29 @@ export const initialHeroes = [
         'publisher':'Marvel Comics', 
         'alter_ego':'Scott Summers',
         'first_appearance':'X-Men #1',
-        'characters':'Scott Summers'
+        'characters':'Scott Summers',
+        'imageUrl': './assets/heroes/marvel-cyclops.jpg'
     },
     {
-        'id': 'marvel-silver',
-        'superhero':'Silver Surfer', 
+        'id': 'marvel-iron',
+        'superhero':'Iron Man', 
         'publisher':'Marvel Comics', 
-        'alter_ego':'Norrin Radd',
-        'first_appearance':'The Fantastic Four #48',
-        'characters':'Norrin Radd'
-    }
+        'alter_ego':'Tony Stark',
+        'first_appearance':'Tales of Suspense #39',
+        'characters':'Tony Stark',
+        'imageUrl': './assets/heroes/marvel-iron.jpg'
+    },
+    {
+        'id': 'marvel-daredevil',
+        'superhero':'Daredevil', 
+        'publisher':'Marvel Comics', 
+        'alter_ego':'Matthew Michael Murdock',
+        'first_appearance':'Daredevil #1',
+        'imageUrl': './assets/heroes/marvel-daredevil.jpg'
+    },
+
+
+
 ];
 
 export default initialHeroes;
